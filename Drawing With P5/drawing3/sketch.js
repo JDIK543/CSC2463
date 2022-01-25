d70cbf0d86fc0b48b882150c67a3f9926356933e
JDIK543 <64238989+JDIK543@users.noreply.github.com>
2022-01-25 16:50:33 -0600

    setup files for each

---
+++ Drawing With P5/drawing3/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(800, 400);
+  }
+  
+  function draw() {
+    background(78, 222, 87);
+    strokeWeight(2);
+    circle(200,200,350);
+    rect(450,40,325,320);
+  }
