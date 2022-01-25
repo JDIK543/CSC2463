d22b262ba3552d2ca02a165207526ab12eef4916
JDIK543 <64238989+JDIK543@users.noreply.github.com>
2022-01-25 16:46:48 -0600

    Update sketch.js

--- Drawing With P5/drawing1/sketch.js
+++ Drawing With P5/drawing1/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
   }
   
   function draw() {
-    background(220);
+    background(78, 222, 87);
+    strokeWeight(2);
+    circle(200,200,350);
+    rect(450,40,325,320);
   }
